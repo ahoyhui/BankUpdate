664bd1f57539600b82b0c84e34b51d953be778f6
Keith Hui <ahoyhui123@gmail.com>
2018-12-07 23:25:34 +0000

    Delete Function done

---
+++ src/main/java/com/mycompany/mavenproject1/Account.java
@@ -0,0 +1,93 @@
+package com.mycompany.mavenproject1;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table
+@XmlRootElement
+public class Account implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private int userNum;
+    private String address;
+    private String email;
+    private int phone;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the radius
+     */
+    public int getUserNum() {
+        return userNum;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @param radius the radius to set
+     */
+    public void setUserNum(int userNum) {
+        this.userNum = userNum;
+    }
+    
+
+   
+}
